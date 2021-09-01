24612a0a60005019840a5e2d6248168e109eb6ac
Myuki <myuki@outlook.ie>
2021-09-01 12:26:50 +0800

    Improve subprocess error handle

--- android-debloater.py
+++ android-debloater.py
@@ -3,7 +3,7 @@
 import getopt
 import subprocess
 import sys
-from typing import Set
+from typing import Dict, Set
 
 if __name__ == "__main__":
   # Default arguments
@@ -17,8 +17,13 @@ if __name__ == "__main__":
   # Get user list
   userList: Set[str] = set()
   command = "adb shell pm list users"
-  output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-  for line in output.splitlines()[1:]:
+  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  process.wait()
+  if process.returncode != 0:
+    print("Error:")
+    print(process.stdout.read().decode().strip())
+    sys.exit(1)
+  for line in process.stdout.read().decode().splitlines()[1:]:
     if "UserInfo{" in line:
       userList.add(line[line.index("{") + 1:line.index(":")])
 
@@ -30,7 +35,7 @@ if __name__ == "__main__":
       if argument.isdigit() and argument in userList:
         user = argument
       else:
-        print("Wrong user ID: " + argument)
+        print("Error: Wrong user ID: " + argument)
         sys.exit(1)
     elif option in ("-a", "--adb"):
       adbPath = argument
@@ -38,11 +43,14 @@ if __name__ == "__main__":
       packageListFilePath = argument
 
   # Parse option
+  if len(arguments) == 0:
+    print("Error: Empty argument!")
+    sys.exit(1)
   pmOption: str = arguments[0]
   if pmOption == "disable":
     pmOption = "disable-user"
   if pmOption not in availablePmOption:
-    print("Wrong option: " + pmOption)
+    print("Error: Wrong option: " + pmOption)
     print("Support option: disable-user(disable), clear, disable-clear, enable")
     sys.exit(1)
 
@@ -52,21 +60,35 @@ if __name__ == "__main__":
     with open(packageListFilePath, "r") as file:
       for line in file.readlines():
         packageList.add(line.strip())
-      file.close()
   else:
     packageList = set(arguments[1:])
+  if packageList == set():
+    print("Error: Empty package!")
+    sys.exit(1)
 
   # Run command
+  processAdb: Dict[str, subprocess.Popen] = {}
   for package in packageList:
-    print(f"{pmOption.capitalize()} {package}:")
+    print(f"{pmOption.capitalize()} {package}...")
     if pmOption == "disable-clear":
       command = f"{adbPath} shell pm disable-user --user {user} {package}"
-      output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-      print(output.strip())
-      command = f"{adbPath} shell pm clear --user --user {user} {package}"
-      output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-      print(f"Clear: {output}" )
+      processAdb[f"disable-user {package} for user {user}"] = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+      command = f"{adbPath} shell pm clear --user {user} {package}"
+      processAdb[f"clear {package} for user {user}"] = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     else:
       command = f"{adbPath} shell pm {pmOption} --user {user} {package}"
-      output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-      print(output)
+      processAdb[f"{pmOption} {package} for user {user}"] = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+  # Wait all adb subprocess
+  error = False
+  for op, process in processAdb.items():
+    process.wait()
+    # Print ADB output if return code is not 0
+    if process.returncode != 0:
+      error = True
+      print(f"Error: Fail to {op}:")
+      print(process.stdout.read().decode().strip())
+  if error:
+    print("Complete with error!\n")
+  else:
+    print("Complete!\n")
